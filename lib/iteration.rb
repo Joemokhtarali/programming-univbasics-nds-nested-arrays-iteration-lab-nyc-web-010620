9519fb9e7587d15c4fc477aebe2f6db592a881a0
Mokhtar Ali <joemokhtarali@gmail.com>
2019-12-25 15:26:33 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,7 +7,7 @@ def join_ingredients(src)
     array.push("I love #{first} and #{second} on my pizza")
   end 
   
-  return array.join(" ")
+  return array
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
   # Build a new Array that contains strings where each pair of foods is
